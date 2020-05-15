55a1107d22daaa06c21c67f5bc4cc740940b1fda
卞瑞江 <bianruijiang@znlh.com>
2020-05-15 10:48:25 +0800

    init project

---
+++ dist/baidu/static/js/npm/chameleon-ui-builtin/components/native-scroller/native-scroller.js
@@ -0,0 +1,51 @@
+var __CML__GLOBAL = require("../../../../manifest.js");
+__CML__GLOBAL.webpackJsonp([17],{
+
+/***/ "../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/babel-loader/lib/index.js?{\"filename\":\"D:\\\\Program Files\\\\nodejs\\\\node_global\\\\node_modules\\\\chameleon-tool\\\\chameleon.js\"}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=script&index=0&fileType=component&media=dev&cmlType=baidu&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./node_modules/chameleon-ui-builtin/components/native-scroller/native-scroller.baidu.cml":
+/***/ (function(module, exports, __webpack_require__) {
+
+Object.defineProperty(exports, "__esModule", {
+      value: true
+});
+
+var _chameleonRuntime = __webpack_require__("./node_modules/chameleon-runtime/index.js");
+
+var _chameleonRuntime2 = _interopRequireDefault(_chameleonRuntime);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var NativeScroller = function NativeScroller() {
+      _classCallCheck(this, NativeScroller);
+};
+
+exports.default = new NativeScroller();
+
+
+module.exports = function () {
+      _chameleonRuntime2.default.createComponent(exports.default).getOptions();
+};
+
+/***/ }),
+
+/***/ "../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/cml-extract-css-webpack-plugin/dist/loader.js?{\"omit\":1,\"remove\":true}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/vue-style-loader/index.js!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/css-loader/index.js?{\"sourceMap\":false}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"platform\":\"miniapp\",\"cmlType\":\"baidu\"}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/postcss-loader/lib/index.js?{\"sourceMap\":false,\"config\":{\"path\":\"D:\\\\Program Files\\\\nodejs\\\\node_global\\\\node_modules\\\\chameleon-tool\\\\configs\\\\postcss\\\\baidu\\\\.postcssrc.js\"}}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/less-loader/dist/cjs.js?{\"sourceMap\":false}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"media\":true,\"cmlType\":\"baidu\"}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=styles&index=0&fileType=component&media=dev&cmlType=baidu&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./node_modules/chameleon-ui-builtin/components/native-scroller/native-scroller.baidu.cml":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./node_modules/chameleon-ui-builtin/components/native-scroller/native-scroller.baidu.cml":
+/***/ (function(module, exports, __webpack_require__) {
+
+var __cml__style0 = __webpack_require__("../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/cml-extract-css-webpack-plugin/dist/loader.js?{\"omit\":1,\"remove\":true}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/vue-style-loader/index.js!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/css-loader/index.js?{\"sourceMap\":false}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"platform\":\"miniapp\",\"cmlType\":\"baidu\"}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/postcss-loader/lib/index.js?{\"sourceMap\":false,\"config\":{\"path\":\"D:\\\\Program Files\\\\nodejs\\\\node_global\\\\node_modules\\\\chameleon-tool\\\\configs\\\\postcss\\\\baidu\\\\.postcssrc.js\"}}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/less-loader/dist/cjs.js?{\"sourceMap\":false}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-css-loader/index.js?{\"media\":true,\"cmlType\":\"baidu\"}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=styles&index=0&fileType=component&media=dev&cmlType=baidu&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./node_modules/chameleon-ui-builtin/components/native-scroller/native-scroller.baidu.cml");
+var __cml__script = __webpack_require__("../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/babel-loader/lib/index.js?{\"filename\":\"D:\\\\Program Files\\\\nodejs\\\\node_global\\\\node_modules\\\\chameleon-tool\\\\chameleon.js\"}!../../../Program Files/nodejs/node_global/node_modules/chameleon-tool/node_modules/chameleon-loader/src/selector.js?type=script&index=0&fileType=component&media=dev&cmlType=baidu&isInjectBaseStyle=true&check={\"enable\":true,\"enableTypes\":[]}!./node_modules/chameleon-ui-builtin/components/native-scroller/native-scroller.baidu.cml");
+
+      __CML__GLOBAL.__CMLCOMPONNETS__['npm/chameleon-ui-builtin/components/native-scroller/native-scroller'] = __cml__script;
+
+
+/***/ })
+
+},["./node_modules/chameleon-ui-builtin/components/native-scroller/native-scroller.baidu.cml"])
+module.exports = __CML__GLOBAL.__CMLCOMPONNETS__['npm/chameleon-ui-builtin/components/native-scroller/native-scroller'];
